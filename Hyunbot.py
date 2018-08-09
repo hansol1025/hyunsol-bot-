c2441e27dde84bf22a718e3605e2125b76c6e0fe
hansol1025 <35056627+hansol1025@users.noreply.github.com>
2018-08-09 18:00:04 +0900

    Update Hyunbot.py

--- Hyunbot.py
+++ Hyunbot.py
@@ -13,10 +13,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.content.startswith('안녕'):
-		chat = random.choice(['안녕!', 'ㅎㅇㅎㅇ', '반가워', '안녕', '왜 왔어!, '그래, 'ㅇㅇ', '응', '왜이리 늦었어', '어디 갔다 옴?'])
+		chat = random.choice(['안녕!', 'ㅎㅇㅎㅇ', '반가워', '안녕', '왜 왔어!', '그래', 'ㅇㅇ', '응', '왜이리 늦었어', '어디 갔다 옴?'])
 		await client.send_message(message.channel, chat)
 	if message.content.startswith('현솔아 몇살이야'):
-		chat = random.choice(['안녕 나는 열두살이야', '반가워 나는 12살이야 초5 임', '반가워', '안녕', '왜 왔어!, '그래, 'ㅇㅇ', '응', '왜이리 늦었어', '어디 갔다 옴'
-		await client.send_message(message.channel, '안녕!')
+		chat = random.choice(['안녕 나는 열두살이야', '반가워 나는 12살이야 초5 임', '초5 인데', '12!', '열두살', '12', '초등학교 5학년인데', '12살이다, 왜!', '곧있으면 13살이다!!', '꼭 말해줘야 하나..'])
+		await client.send_message(message.channel, chat)
 				      
 client.run(os.getenv('TOKEN'))
