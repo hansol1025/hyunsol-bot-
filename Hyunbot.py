8f8bbd7af1aaa99ace98e807fe495541249f79ac
hansol1025 <35056627+hansol1025@users.noreply.github.com>
2018-08-09 17:46:22 +0900

    Update Hyunbot.py

--- Hyunbot.py
+++ Hyunbot.py
@@ -12,7 +12,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('테스트'):
+	if message.content.startswith('안녕'):
+		chat = random.choice(['안녕!', 'ㅎㅇㅎㅇ', '반가워', '안녕', '왜 왔어!, '그래, 'ㅇㅇ', '응', '왜이리 늦었어', '어디 갔다 옴?'])
+		await client.send_message(message.channel, chat)
+	if message.content.startswith('현솔아 몇살이야'):
+		chat = random.choice(['안녕 나는 열두살이야', '반가워 나는 12살이야 초5 임', '반가워', '안녕', '왜 왔어!, '그래, 'ㅇㅇ', '응', '왜이리 늦었어', '어디 갔다 옴'
 		await client.send_message(message.channel, '안녕!')
-
+				      
 client.run(os.getenv('TOKEN'))
